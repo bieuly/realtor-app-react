f8c7ea680d8f9687cb0036a39b0d80f3e2eb8f49
Billy Hoang <billy-hoang@hotmail.com>
2018-07-27 01:47:55 -0700

    Test out side bar component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import FavoriteListings from './components/FavoriteListings';
 import RealtyProvider from './providers/RealtyProvider';
 import Filter from './components/Filter';
 import { Loader } from 'semantic-ui-react'
+import { Button, Icon, Menu, Segment, Sidebar } from 'semantic-ui-react'
 
 class App extends Component {
 
@@ -18,7 +19,8 @@ class App extends Component {
         option: "id",
         searchTerm: ""
       },
-      favorites: new Set()
+      favorites: new Set(),
+      visible: false
     }
 
     this.realtyProvider = new RealtyProvider();
@@ -80,7 +82,12 @@ class App extends Component {
     return favorites;
   }
 
+  handleButtonClick = () => this.setState({ visible: !this.state.visible })
+
+  handleSidebarHide = () => this.setState({ visible: false })
+
   render() {
+    const { visible } = this.state
     const listingsToDisplay = this.filterListings();
     const favoritesToDisplay = this.getFavoriteListings();
 
@@ -89,11 +96,40 @@ class App extends Component {
     } else {
       return (
         <div className="App">
+        <Button onClick={this.handleButtonClick}>Toggle visibility</Button>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            icon='labeled'
+            inverted
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={visible}
+            width='thin'
+          >
+            <Menu.Item as='a'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>  
+
+          <Sidebar.Pusher>
           <h1>Realtor App</h1>
           <Filter updateFilter={this.updateFilter}/>
           <MainListings listings={listingsToDisplay} favorites={this.state.favorites} toggleFavorites={this.toggleFavorites}/>
           <h1>Favorites</h1>
           <FavoriteListings listings={favoritesToDisplay} toggleFavorites={this.toggleFavorites}/>
+          </Sidebar.Pusher>
+          </Sidebar.Pushable>
         </div>
       );
     }
