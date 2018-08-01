085109dceeefd6c66c126584285bc8928871b6c7
Billy Hoang <billy-hoang@hotmail.com>
2018-08-01 00:12:25 -0700

    Implement title screen with transitions

--- src/App.js
+++ src/App.js
@@ -1,74 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import './App.css';
-import MainListings from './components/MainListings';
-import FavoriteListings from './components/FavoriteListings';
-import RealtyProvider from './providers/RealtyProvider';
-import Filter from './components/Filter';
-import { Loader } from 'semantic-ui-react'
+import { Transition, Icon } from 'semantic-ui-react'
+import RealtyApp from './RealtorApp'
 
-class App extends Component {
-
-  constructor(props) {
-    
-    super(props)
-
-    this.state = {
-      realtyData: [],
-      favorites: new Set()
-    }
-
-    this.realtyProvider = new RealtyProvider();
-  }
-
-  componentDidMount() {
-    this.realtyProvider.getRealtyData().then(data=>{
-      this.setState({realtyData: data})
-    });
-  }
-
-  toggleFavorites = (id) => {
-    if(!this.state.favorites.has(id)){
-      this.setState((prevState)=>{
-        prevState.favorites.add(id)
-        const newFavorites = new Set([...prevState.favorites])
-        return {
-          favorites: newFavorites
+export default class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            hover: true,
+            showApp: false
         }
-      })
-    } else {
-      this.setState((prevState)=>{
-        prevState.favorites.delete(id)
-        const newFavorites = new Set([...prevState.favorites])
-        return {
-          favorites: newFavorites
+    }
+
+    handleOnHover = () => {
+        console.log("heuy")
+        this.setState((prevState)=>{
+            return {
+                hover: !prevState.hover
+            }
+        })
+    }
+
+    handleOnClick = () => {
+        this.setState({showApp: true})
+    }
+
+    render() {
+        if(this.state.showApp) {
+            return <RealtyApp/>
         }
-      })
+        return (
+            <div id="titleScreen" className="App App-title-header">
+                {/* Why can't I provide class here for css on h1? */}
+                <Transition animation="fade up" transitionOnMount={true} duration={1600}>
+                <h1>
+                    Welcome.
+                    <Transition animation="tada" visible={this.state.hover} duration={550}>
+                        <Icon name="building outline" size="large" onClick={()=>console.log("hi")} onMouseOver={this.handleOnHover} onClick={this.handleOnClick}/>
+                    </Transition>
+                    </h1>
+                </Transition>
+            </div>
+        );
     }
-  }
-
-  getFavoriteListings = () => {
-    const favorites =  this.state.realtyData.filter((listing)=>{
-        return this.state.favorites.has(listing.id)
-      })
-    return favorites;
-  }
-
-  render() {
-    const favoritesToDisplay = this.getFavoriteListings();
-
-    if(!this.state.realtyData.length){
-      return <Loader size="large" active>Fetching Data</Loader>
-    } else {
-      return (
-        <div className="App">
-          <h1>Realtor App</h1>
-          <MainListings listings={this.state.realtyData} favorites={this.state.favorites} toggleFavorites={this.toggleFavorites}/>
-          <h1>Favorites</h1>
-          <FavoriteListings listings={favoritesToDisplay} toggleFavorites={this.toggleFavorites}/>
-        </div>
-      );
-    }
-  }
 }
-
-export default App;
